4e7f09fbbb30cf04555e795aba5c49d9158baa44
Scott Zockoll <32425272+scottzockoll@users.noreply.github.com>
2020-11-28 00:31:42 -0500

    Update tensor_test.py (#6)

--- test/tensor_test.py
+++ test/tensor_test.py
@@ -1,14 +1,14 @@
-from deep_learning.tensor import Tensor
+import deep_learning as dl
 from deep_learning.utils import zeros, ones
 from .testing_tools import get_positive_tuple
 
-t1 = Tensor([1, 2, 3])
-t2 = Tensor([1, 1, 1])
-m1 = Tensor([
+t1 = dl.Tensor([1, 2, 3])
+t2 = dl.Tensor([1, 1, 1])
+m1 = dl.Tensor([
     [1, 2, 3],
     [4, 5, 6]
 ])
-m1_T = Tensor([
+m1_T = dl.Tensor([
     [1, 4],
     [2, 5],
     [3, 6]
@@ -16,22 +16,23 @@ m1_T = Tensor([
 
 
 def test_eq():
-    assert Tensor([]) == Tensor([])
-    assert Tensor([]) != Tensor([0])
-    assert t1 == Tensor([1, 2, 3])
-    assert t1 != Tensor([0, 2, 3])
+    assert dl.Tensor([]) == dl.Tensor([])
+    assert dl.Tensor([]) != dl.Tensor([0])
+    assert t1 == dl.Tensor([1, 2, 3])
+    assert t1 != dl.Tensor([0, 2, 3])
     shape = get_positive_tuple()
     assert zeros(shape) == zeros(shape)
     assert ones(shape) != zeros(shape)
 
 
 def test_sub():
-    assert t1 - t2 == Tensor([0, 1, 2])
+    assert t1 - t2 == dl.Tensor([0, 1, 2])
 
 
 def test_T():
     assert m1.T == m1_T
 
 
-def test_oops():
-    assert True
+def test_len():
+    assert len(dl.Tensor([])) == 0
+    assert len(t1) == 3
